addfd65a7816338292ce19a72e69a7ab0f56b49a
Rohit Agarwal <rohitagarwal@startbitsolutions.com>
2025-10-03 15:41:39 +0530

    Add Weekly, Monthly, Last Day of Month Job

--- TaskSchedulerQuartz/Scheduler/QuartzJobScheduler.cs
+++ TaskSchedulerQuartz/Scheduler/QuartzJobScheduler.cs
@@ -5,6 +5,9 @@ namespace TaskSchedulerQuartz.Scheduler
 {
     public static class QuartzJobScheduler
     {
+        //Note: Quartz cron expressions have 6 (or 7) fields:
+        //Seconds  Minutes  Hours  DayOfMonth  Month  DayOfWeek  [Year - optional]
+
         public static void ConfigureJobs(IServiceCollectionQuartzConfigurator q)
         {
             // Email Job - Runs daily at 3:00 PM
@@ -13,23 +16,23 @@ namespace TaskSchedulerQuartz.Scheduler
             q.AddTrigger(opts => opts
                 .ForJob(emailJobKey)
                 .WithIdentity("EmailJob-trigger")
-                .WithCronSchedule("0 7 15 * * ?"));
+                .WithCronSchedule("0 0 15 * * ?"));
 
-            // Report Job - Runs daily at 3:01 PM
-            var reportJobKey = new JobKey("ReportJob");
-            q.AddJob<ReportJob>(opts => opts.WithIdentity(reportJobKey));
-            q.AddTrigger(opts => opts
-                .ForJob(reportJobKey)
-                .WithIdentity("ReportJob-trigger")
-                .WithCronSchedule("0 8 15 * * ?"));
-
-            // Backup Job - Runs daily at 3:02 PM
+            // Weekly Job (Every Friday at 3:00 PM)
             var backupJobKey = new JobKey("BackupJob");
             q.AddJob<BackupJob>(opts => opts.WithIdentity(backupJobKey));
             q.AddTrigger(opts => opts
                 .ForJob(backupJobKey)
                 .WithIdentity("BackupJob-trigger")
-                .WithCronSchedule("0 9 15 * * ?"));
+                .WithCronSchedule("0 0 15 ? * FRI"));
+
+            // Monthly Job (30th of each month at 3:00 PM)
+            var reportJobKey = new JobKey("ReportJob");
+            q.AddJob<ReportJob>(opts => opts.WithIdentity(reportJobKey));
+            q.AddTrigger(opts => opts
+                .ForJob(reportJobKey)
+                .WithIdentity("ReportJob-trigger")
+                .WithCronSchedule("0 0 15 30 * ?"));
 
             // One-time Special Job (Oct 10, 2025 at 3:00 PM)
             var specialJobKey = new JobKey("SpecialJob");
@@ -39,6 +42,15 @@ namespace TaskSchedulerQuartz.Scheduler
                 .WithIdentity("SpecialJob-trigger")
                 .StartAt(new DateTimeOffset(new DateTime(2025, 10, 03, 15, 15, 0)))
                 .WithSimpleSchedule(x => x.WithRepeatCount(0)));
+
+            // Last Day of Month Job (3:00 PM)
+            var lastDayJobKey = new JobKey("LastDayOfMonthJob");
+            q.AddJob<LastDayOfMonthJob>(opts => opts.WithIdentity(lastDayJobKey));
+            q.AddTrigger(opts => opts
+                .ForJob(lastDayJobKey)
+                .WithIdentity("LastDayOfMonthJob-trigger")
+                .WithCronSchedule("0 0 15 L * ?"));
+
         }
     }
 }
---
+++ TaskSchedulerQuartz/Services/JobServices/LastDayOfMonthJob.cs
@@ -0,0 +1,13 @@
+﻿using Quartz;
+
+namespace TaskSchedulerQuartz.Services.JobServices
+{
+    public class LastDayOfMonthJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("📅 Last day of month job executed at " + DateTime.Now);
+            await Task.CompletedTask;
+        }
+    }
+}
