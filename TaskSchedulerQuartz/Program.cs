9901477f400abc1584fc08f73ea8c3b6aaa502b3
Rohit Agarwal <rohitagarwal@startbitsolutions.com>
2025-10-03 15:04:05 +0530

    First Test

--- TaskSchedulerQuartz/Program.cs
+++ TaskSchedulerQuartz/Program.cs
@@ -1,3 +1,5 @@
+using Quartz;
+using TaskSchedulerQuartz.Services.JobServices;
 
 namespace TaskSchedulerQuartz
 {
@@ -8,6 +10,37 @@ namespace TaskSchedulerQuartz
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            // Add Quartz services
+            builder.Services.AddQuartz(q =>
+            {
+
+                // Schedule EmailJob at 3:00 PM daily
+                var emailJobKey = new JobKey("EmailJob");
+                q.AddJob<EmailJob>(opts => opts.WithIdentity(emailJobKey));
+                q.AddTrigger(opts => opts
+                    .ForJob(emailJobKey)
+                    .WithIdentity("EmailJob-trigger")
+                    .WithCronSchedule("0 59 14 * * ?")); // 3:00 PM IST daily
+
+                // Schedule ReportJob at 3:01 PM daily
+                var reportJobKey = new JobKey("ReportJob");
+                q.AddJob<ReportJob>(opts => opts.WithIdentity(reportJobKey));
+                q.AddTrigger(opts => opts
+                    .ForJob(reportJobKey)
+                    .WithIdentity("ReportJob-trigger")
+                    .WithCronSchedule("0 0 15 * * ?")); // 3:01 PM
+
+                // Schedule BackupJob at 3:02 PM daily
+                var backupJobKey = new JobKey("BackupJob");
+                q.AddJob<BackupJob>(opts => opts.WithIdentity(backupJobKey));
+                q.AddTrigger(opts => opts
+                    .ForJob(backupJobKey)
+                    .WithIdentity("BackupJob-trigger")
+                    .WithCronSchedule("0 1 15 * * ?")); // 3:02 PM
+            });
+
+            // Add Quartz Hosted Service
+            builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ TaskSchedulerQuartz/Services/JobServices/EmailJob.cs
@@ -0,0 +1,20 @@
+﻿using Quartz;
+
+namespace TaskSchedulerQuartz.Services.JobServices
+{
+    public class EmailJob : IJob
+    {
+        private readonly ILogger<EmailJob> _logger;
+
+        public EmailJob(ILogger<EmailJob> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Email Job executed at {time}", DateTime.Now);
+            return Task.CompletedTask;
+        }
+    }
+}
